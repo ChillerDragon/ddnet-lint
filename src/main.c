c44792595cefdfb2b69da3417470c086edaf6201
ChillerDragon <chillerdragon@gmail.com>
2025-12-04 15:18:25 +0100

    refactor check files func

--- src/main.c
+++ src/main.c
@@ -3,6 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void check_files(const char * const filenames[], int num_filenames, const char * const cli_args[], int num_cli_args) {
+	int errors = 0;
+
+	for(int i = 0; i < num_filenames; i++) {
+		const char * base_filename = filenames[i];
+		char source_filename[512];
+		char header_filename[512];
+		snprintf(header_filename, sizeof(header_filename), "%s.h", base_filename);
+		snprintf(source_filename, sizeof(source_filename), "%s.cpp", base_filename);
+		ddl_check_src_and_header(header_filename, source_filename, cli_args, num_cli_args);
+
+		if(!ddl_check_floats(source_filename)) {
+			errors++;
+		}
+		if(!ddl_check_floats(header_filename)) {
+			errors++;
+		}
+	}
+
+	if(errors) {
+		fprintf(stderr, "Failed with %d errors!\n", errors);
+		exit(1);
+	}
+}
+
 int main(int argc, const char **argv) {
 	if (argc < 1) {
 		// printf("Usage: %s [clang_args...]\n", argv[0]);
@@ -27,29 +52,8 @@ int main(int argc, const char **argv) {
 		"src/base/fs",
 		"src/base/windows"
 	};
-
-	int errors = 0;
-
-	for(int i = 0; i < sizeof(filenames) / sizeof(const char *); i++) {
-		const char *base_filename = filenames[i];
-		char source_filename[512];
-		char header_filename[512];
-		snprintf(header_filename, sizeof(header_filename), "%s.h", base_filename);
-		snprintf(source_filename, sizeof(source_filename), "%s.cpp", base_filename);
-		ddl_check_src_and_header(header_filename, source_filename, cli_args, num_cli_args);
-
-		if(!ddl_check_floats(source_filename)) {
-			errors++;
-		}
-		if(!ddl_check_floats(header_filename)) {
-			errors++;
-		}
-	}
-
-	if(errors) {
-		fprintf(stderr, "Failed with %d errors!\n", errors);
-		exit(1);
-	}
+	int num_filenames = sizeof(filenames) / sizeof(const char *);
+	check_files(filenames, num_filenames, cli_args, num_cli_args);
 
 	puts("OK");
 	return 0;
